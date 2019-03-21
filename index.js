b05a5c8b2c8e540f4f29f27bfc377f02bdb0ddb2
Sam Parkinson <parkinsonsp42@gmail.com>
2019-03-21 19:51:25 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// todo: make calls to API, manipulate API data, present results
+
+function submitClicked() {
+  // this function will take user input from the search bar
+  // erase previous results if any exist
+  // call functions to reach out to APIs for data
+}
+
+$(submitClicked);
