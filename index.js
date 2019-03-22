a782d73be5ff3b570470f8ce78050b58013fbe4c
Sam Parkinson <parkinsonsp42@gmail.com>
2019-03-22 14:19:14 -0400

    implement search of recipe API

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 'use strict';
 
-// todo: make calls to API, manipulate API data, present results
+function getRecipe(food) {
+  console.log('getting recipes...');
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`)
+    .then(response => response.json())
+    .then(responseJson => addRecipes(responseJson));
+// maybe some kind of catching thing to prevent errors w/ recipe API?
+}
+
+function getRestaurant(food) {
+  console.log(`Currently, functionality to find restaurants serving ${food} is not implemented.`)
+  // go to restaurant API, get nearby restaurants
+  // call function to check restaurant menus
+  // write out logic for how this works
+}
 
 function submitClicked() {
-  // this function will take user input from the search bar
-  // erase previous results if any exist
-  // call functions to reach out to APIs for data
+  $('form').submit(event => {
+    event.preventDefault();
+    console.log('submit clicked');
+    const food = $(this).find('#searchbar').val();
+    getRecipe(food);
+    getRestaurant(food);
+  })
 }
 
 $(submitClicked);
