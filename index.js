1a00c6b6b8871ec7603dc00bef73698af632c99c
Sam Parkinson <parkinsonsp42@gmail.com>
2019-03-22 18:48:48 -0400

    log most needed recipe info to console

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 'use strict';
 
+// TODO, if possible: error code that returns different possibilities for three possible error states
+// no recipe, but restaurant
+// recipe, no restaurant
+// neither of them
+
 function getRecipe(food) {
   console.log('getting recipes...');
   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`)
     .then(response => response.json())
     .then(responseJson => addRecipes(responseJson));
 // maybe some kind of catching thing to prevent errors w/ recipe API?
+// see TODO notes for logic on this
 }
 
 function getRestaurant(food) {
@@ -13,6 +19,22 @@ function getRestaurant(food) {
   // go to restaurant API, get nearby restaurants
   // call function to check restaurant menus
   // write out logic for how this works
+}
+
+function addRecipes(recipeJson) {
+  const recData = recipeJson.meals;
+  console.log(recData);
+  for (let i = 0; i < recData.length; i++) {
+    console.log(recData[i].strMeal);
+    console.log(recData[i].strInstructions);
+    let j = 1;
+    while (recData[i][`strIngredient${j}`] != '') {
+      console.log(recData[i][`strIngredient${j}`]);
+      console.log(recData[i][`strMeasure${j}`]);
+      j++;
+    }
+  }
+  // add the recipes to the DOM
 }
 
 function submitClicked() {
