08a9257713e367cd1c0b008a413fe78e5e946062
Sam Parkinson <parkinsonsp42@gmail.com>
2019-04-02 18:57:36 -0400

    add desktop styling

--- index.js
+++ index.js
@@ -5,46 +5,6 @@
 // recipe, no restaurant
 // neither of them
 
-function divClicked() {
-  $('.container').click(function() {
-    $('.container').removeClass('default focused').addClass('unfocused');
-    $(this).removeClass('unfocused').addClass('focused');
-  })
-}
-
-function submitClicked() {
-  $('form').submit(event => {
-    event.preventDefault();
-    $('.container').removeClass('focused unfocused hidden').addClass('default');
-    $('#recipes ul, #restaurants ul').empty();
-    const food = $(this).find('#searchbar').val();
-    getCategory(food);
-    getLocation(food);
-    
-    // return divs to their default sizes, maybe toggle categories for this?
-  })
-}
-
-function getCategory(food) {
-  console.log('getting recipes...');
-  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${food}`)
-    .then(response => response.json())
-    .then(responseJson => getRecipes(responseJson));
-  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${food}`)
-    .then(response => response.json())
-    .then(responseJson => getRecipes(responseJson));
-// maybe some kind of catching thing to prevent errors w/ recipe API?
-// see TODO notes for logic on this
-}
-
-function getRecipes(responseJson) {
-  for (let i = 0; i < responseJson.meals.length; i++){
-    fetch (`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${responseJson.meals[i].idMeal}`)
-      .then(meal => meal.json())
-      .then(mealJson => addRecipes(mealJson));
-  }
-}
-
 function addRecipes(mealJson) {
   $('#recipes ul').append(
     `<li>${mealJson.meals[0].strMeal}
@@ -61,44 +21,24 @@ function addRecipes(mealJson) {
     }
 }
 
-function getLocation(food) {
-  navigator.geolocation.getCurrentPosition(position => {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    findNearbyRestaurants(lat, lon, food);
-  });
+function getRecipes(responseJson) {
+  for (let i = 0; i < responseJson.meals.length; i++){
+    fetch (`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${responseJson.meals[i].idMeal}`)
+      .then(meal => meal.json())
+      .then(mealJson => addRecipes(mealJson));
+  }
 }
 
-function findNearbyRestaurants(lat, lon, food) {
-  console.log(`Searching for restaurants near lat ${lat} and lon ${lon}...`)
-  const url = `https://developers.zomato.com/api/v2.1/cuisines?lat=${lat}&lon=${lon}`
-  const options = {
-    headers: new Headers({
-      'user-key': `fake-api-key`})
-  }
-  fetch(url, options)
+function getCategory(food) {
+  console.log('getting recipes...');
+  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${food}`)
+    .then(response =>response.json())
+    .then(responseJson => getRecipes(responseJson));
+  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${food}`)
     .then(response => response.json())
-    .then(responseJson => getCuisineID(responseJson, food));
-}
-
-function getCuisineID(responseJson, food, lat, lon) {
-  for (let i = 0; i < responseJson.cuisines.length; i++) {
-    if (responseJson.cuisines[i].cuisine.cuisine_name == food) {
-      const cuisineID = responseJson.cuisines[i].cuisine.cuisine_id;
-      getRestaurants(lat, lon, cuisineID);
-    }
-  }
-}
-
-function getRestaurants(lat, lon, cuisineID) {
-  const url = `https://developers.zomato.com/api/v2.1/search?lat=${lat}&lon=${lon}&radius=10000&cuisines=${cuisineID}`
-  const options = {
-    headers: new Headers({
-      'user-key': `fake-api-key`})
-  }
-  fetch(url, options)
-    .then(response => response.json())
-    .then(responseJson => addRestaurants(responseJson));
+    .then(responseJson => getRecipes(responseJson));
+// maybe some kind of catching thing to prevent errors w/ recipe API?
+// see TODO notes for logic on this
 }
 
 function addRestaurants(responseJson) {
@@ -114,5 +54,63 @@ function addRestaurants(responseJson) {
   }
 }
 
+function getRestaurants(lat, lon, cuisineID) {
+  const url = `https://developers.zomato.com/api/v2.1/search?lat=${lat}&lon=${lon}&radius=10000&cuisines=${cuisineID}`
+  const options = {
+    headers: new Headers({
+      'user-key': `fake-api-key`})
+  }
+  fetch(url, options)
+    .then(response => response.json())
+    .then(responseJson => addRestaurants(responseJson));
+}
+
+function getCuisineID(responseJson, food, lat, lon) {
+  for (let i = 0; i < responseJson.cuisines.length; i++) {
+    if (responseJson.cuisines[i].cuisine.cuisine_name == food) {
+      const cuisineID = responseJson.cuisines[i].cuisine.cuisine_id;
+      getRestaurants(lat, lon, cuisineID);
+    }
+  }
+}
+
+function findNearbyRestaurants(lat, lon, food) {
+  console.log(`Searching for restaurants near lat ${lat} and lon ${lon}...`)
+  const url = `https://developers.zomato.com/api/v2.1/cuisines?lat=${lat}&lon=${lon}`
+  const options = {
+    headers: new Headers({
+      'user-key': `fake-api-key`})
+  }
+  fetch(url, options)
+    .then(response => response.json())
+    .then(responseJson => getCuisineID(responseJson, food));
+}
+
+function getLocation(food) {
+  navigator.geolocation.getCurrentPosition(position => {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    findNearbyRestaurants(lat, lon, food);
+  });
+}
+
+function submitClicked() {
+  $('form').submit(event => {
+    event.preventDefault();
+    $('#recipes ul, #restaurants ul').empty();
+    $('.container').removeClass('focused unfocused hidden').addClass('default');
+    const food = $(this).find('#searchbar').val();
+    getCategory(food);
+    getLocation(food);
+    divClicked();
+  })
+}
+
+function divClicked() {
+  $('.container').click(function() {
+    $('.container').removeClass('default focused').addClass('unfocused');
+    $(this).removeClass('unfocused').addClass('focused');
+  })
+}
+
 $(submitClicked);
-$(divClicked);
